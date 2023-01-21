5a7e13482edcd728026fd56b772a652588aae727
Vladyslav Kupchuk <kupchuk.vlad@gmail.com>
2023-01-21 11:37:23 +0200

    Completed Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+	startBtn: document.querySelector('[data-start]'),
+	stopBtn: document.querySelector('[data-stop]'),
+}
 
+let timerId = null;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', onClickStartBtn);
+
+refs.stopBtn.addEventListener('click', onClickStopBtn);
+
+
+function onClickStopBtn(e) {
+	refs.startBtn.disabled = false;
+	refs.stopBtn.disabled = true;
+	clearInterval(timerId);
+}
+
+function onClickStartBtn(e) {
+	refs.startBtn.disabled = true;
+	refs.stopBtn.disabled = false;
+	addBackgroundColorInBody();
+	timerId = setInterval(addBackgroundColorInBody, 1000);
+}
+
+function addBackgroundColorInBody() {
+	document.body.style.backgroundColor = getRandomHexColor();
+} 
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
