ef91ecff1b1e477d974e4ebc182295190b066974
Vladyslav Kupchuk <kupchuk.vlad@gmail.com>
2023-01-21 19:46:03 +0200

    fixed 2.0 task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,12 +2,15 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
 	formEl: document.querySelector('.form'),
+	submitBtn: document.querySelector('[type = submit]')
 }
 const data = {
+	// isCount: false,
 	delayValue: 0,
 	stepValue: 0,
 	amountValue: 0,
 }
+
 
 refs.formEl.addEventListener('submit', onFormSubmit);
 refs.formEl.addEventListener('change', onFormChange);
@@ -28,17 +31,25 @@ function onFormChange(e) {
 
 function onFormSubmit(e) {
 	e.preventDefault();
+	refs.submitBtn.disabled = true;
 	let saveDelay = data.delayValue;
 	for (let i = 0; i < data.amountValue; i += 1) {
 		createPromise(i + 1, saveDelay)
 			.then(({ position, delay }) => {
 				Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+				if (position === data.amountValue) {
+					refs.submitBtn.disabled = false;
+				}
 			})
 			.catch(({ position, delay }) => {
 				Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+				if (position === data.amountValue) {
+					refs.submitBtn.disabled = false;
+				}
 			});
 		saveDelay += data.stepValue;
 	}
+	
 }
 
 function createPromise(position, delay) {
